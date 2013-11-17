7d99ca75a226bb52b9aa96b5e32cde867e98113a
Dan Crews <dancrews@me.com>
2013-11-16 19:13:47 -0500

    push to Guthub

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Events::Application.config.secret_key_base = '89957fb5a9db828b16c20426c92ea4b751b4534a56050db2b01f45814b01f14a34aa4c8ebaa6929fd49e157666fd572e5c51a05fe5728192f020d7f77b978a6a'
