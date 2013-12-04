9199617300022afbce2487e1fb6c023548e6cb61
Dan Crews <dancrews@me.com>
2013-12-04 17:41:11 -0500

    Added new events page and create action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Events::Application.routes.draw do
 root "events#index"
-get "events" => "events#index"
-get "events/:id" =>"events#show", as: "event"
-get "events/:id/edit" =>"events#edit", as: "edit_event"
+resources :events
 end
